6de1a4cc02f74eb91caf61b5b1b04a3d06512b61
David Lange <david.lange@cern.ch>
2020-04-05 15:19:34 +0200

    animate gui

--- nurse/__main__.py
+++ nurse/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtCore import QTimer
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import numpy as np
@@ -61,7 +62,7 @@ class PatientSensor(QtWidgets.QWidget):
 
         self.graph = pg.PlotWidget()
         self.graph.setLabel('left', 'Flow', units='L/m')
-        self.graph.setLabel('bottom', 'Time', units='t')
+        self.graph.setLabel('bottom', 'Time', units='seconds')
         layout.addWidget(self.graph)
 
         self.alert = AlertWidget(i)
@@ -78,9 +79,24 @@ class PatientSensor(QtWidgets.QWidget):
         lower_layout.addWidget(QtWidgets.QLabel("AveFlow: 12.2 L/m"), 1, 1)
         lower_layout.addWidget(QtWidgets.QLabel("CurrentFlow: 20 L/m"), 1, 2)
 
-    def set_plot(self, time, values, ok=True):
+        self.num_data_points=1000
+        self.time=np.arange(-1000,0,1)
+        self.flow = (np.mod(self.time + i*13, 100) / 10 - 2) * np.random.uniform(.9,1.1, len(self.time))
+        self.curr_bin=999
+        self.real_time=self.time[-1] # for animation
+
+    def set_plot(self, i ):#time, values, ok=True):
+        #hardwire some error conditions
+        isdead=i==4
+        if isdead: return
+        ok = i%7 != 1
+
+        self.curr_bin = (self.curr_bin + 1) % self.num_data_points
+        self.real_time +=1
+        self.flow[self.curr_bin] = ((self.real_time + i*13 % 100) / 10 - 2) * np.random.uniform(.9,1.1,1)
+        
         pen = pg.mkPen(color=(0, 255, 0) if ok else (255, 0, 0), width=5)
-        self.graph.plot(time, values, pen=pen)
+        self.graph.plot(self.time, np.roll(self.flow,-1*self.curr_bin), pen=pen)
 
         upper = pg.InfiniteLine(angle=0)
         upper.setPos([0,10])
@@ -123,16 +139,21 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.graphs = [PatientSensor(i) for i in range(20)]
         for i, graph in enumerate(self.graphs):
-            layout.addWidget(self.graphs[i], *reversed(divmod(i, 5)))
-        
-        time = np.arange(1000)
+            layout.addWidget(self.graphs[i], *reversed(divmod(i, 5)))       
+            graph.set_plot(i)
+            
+        self.qTimer = QTimer()
+        self.qTimer.setInterval(1000)
+        self.qTimer.timeout.connect(self.update_graphs)
+        self.qTimer.start()
 
+    def update_graphs(self):
+        print("Calling")
         for i, graph in enumerate(self.graphs):
-            if i == 4: continue
-            flow = (np.mod(time + i*13, 100) / 10 - 2) * np.random.uniform(.9,1.1, len(time))
-            self.graphs[i].set_plot(time, flow, i%7 != 1)
-
+            graph.set_plot(i)
+        
 def main():
+    import time
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
@@ -141,3 +162,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
