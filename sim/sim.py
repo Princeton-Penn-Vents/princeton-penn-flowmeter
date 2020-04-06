4d44e0b5b19342b25a58200305c87b9e1ceccea9
David Lange <david.lange@cern.ch>
2020-04-06 16:46:58 +0200

    b-o-e simulation

---
+++ sim/sim.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+import sys
+import numpy
+
+def get_param(key,args,name):
+    if key not in args:
+        print("Missing argument to",name,"is",key)
+        sys.exit(1)
+    return args[key]
+
+def get_param_with_default(key,args,name,default):
+    if key not in args:
+        return default
+    return args[key]
+
+
+def constant_compliance(kwargs):
+    return 0.4
+
+
+def get_breath_starts(**kwargs):
+    """
+    expected parameters:
+    breathing_rate (seconds between breaths)
+    max_time (end time of the simulation)
+
+    returns:
+    array of breath start times
+    """
+
+    max_time = get_param("max_time",kwargs,"get_breath_starts")
+    breathing_rate = get_param("breathing_rate",kwargs,"get_breath_starts")
+    
+    max_breaths = 1.2* max_time / breathing_rate # safety margin for fluctuating this later
+
+    breath_starts = numpy.arange(1,1+max_breaths*breathing_rate,breathing_rate)
+
+    return breath_starts#[breath_starts<max_time]
+    
+    
+def nominal_flow(**kwargs):
+    """
+    expected parameters
+    sim_time, (seconds)
+    sampling_rate (Hz)
+    max_flow, L/minute
+    tidal_volume, L
+    breath_starts, array of timestamps 
+    recovery_tau,
+    inhale_exhale_ratio (optional, default is 0.5)
+    """
+
+    sim_time=get_param("sim_time",kwargs,"nominal_flow")
+    sampling_rate=get_param("sampling_rate",kwargs,"nominal_flow")
+    max_flow=get_param("max_flow",kwargs,"nominal_flow")
+    tidal_volume=get_param("tidal_volume",kwargs,"nominal_flow")
+    breath_starts=get_param("breath_starts",kwargs,"nominal_flow")
+    recovery_tau=get_param("recovery_tau",kwargs,"nominal_flow")
+
+    bins=int(sim_time * sampling_rate)
+    flow = numpy.zeros(bins)
+
+    flow_start_bins = breath_starts * sampling_rate
+    #max_flow is in L/minute
+    flow_end_bins = flow_start_bins + tidal_volume*sampling_rate / (max_flow/60.)
+
+    flow_start_bins = flow_start_bins.astype(int)
+    flow_end_bins = flow_end_bins.astype(int)
+    breath_integrals=numpy.zeros(len(flow_start_bins))
+    
+    for i in range(len(flow_start_bins)):
+        if flow_start_bins[i]<bins:
+            flow[ flow_start_bins[i]:min(bins,flow_end_bins[i]) ] = max_flow
+            breath_integrals[i]=numpy.sum(flow[flow_start_bins[i]:min(bins,flow_end_bins[i])])
+
+    exp_zero_bins = flow_start_bins[1:]
+    exp_min_bins = flow_end_bins[:-1]
+    times=numpy.arange(0,1.2*sim_time,1.0/sampling_rate)
+
+    for i in range(len(exp_min_bins)):
+        if exp_min_bins[i]<bins:
+            b_min=exp_min_bins[i]+2
+            b_max=min(bins,exp_zero_bins[i])-1
+            flow[b_min:b_max] = numpy.log( (times[b_min:b_max] - times[flow_start_bins[i]]) / (times[b_max]-times[flow_start_bins[i]]))
+            flow[b_min:b_max]*=-1.0*breath_integrals[i]/numpy.sum(flow[b_min:b_max])
+            
+    return flow
+
+def nominal_volume(**kwargs):
+    """
+    expected parameters
+    flow
+    v0
+    sample_rate
+    """
+    flow = get_param("flow",kwargs,"nominal_volume")
+    v0 = get_param("v0",kwargs,"nominal_volume")
+    sample_rate = get_param("sample_rate",kwargs,"nominal_volume")
+
+    return numpy.cumsum(flow)*(1.0/sample_rate)+v0
+
+def nominal_pressure(**kwargs):
+    """
+    expected parameters
+    volume
+    peep (cm H20)
+    compliance_func
+    """
+
+    volume = get_param("volume",kwargs,"nominal_pressure")
+    peep = get_param("peep",kwargs,"nominal_pressure")
+    compliance = get_param("compliance_func",kwargs,"nominal_pressure")
+    
+    pressure= numpy.zeros(len(volume))
+    pressure[0]=peep
+    compliance_val= compliance(kwargs)
+    delta_p = (volume[1:]-volume[:-1])*compliance_val
+    pressure[1:]=pressure[0]+numpy.cumsum(delta_p)
+    return pressure
+
+if __name__ == "__main__":
+    sim_time = 120.
+    sample_rate = 100.
+    breathing_rate = 12.
+    max_flow = 12. # L/m
+    tidal_volume = 0.6 # L
+    peep = 4 #cm H20
+
+    breaths=get_breath_starts( max_time=sim_time, breathing_rate=breathing_rate)
+
+    flow = nominal_flow(sim_time=sim_time,
+                        sampling_rate= sample_rate,
+                        max_flow = max_flow,
+                        tidal_volume = tidal_volume,
+                        breath_starts = breaths,
+                        recovery_tau = 15.0)
+
+    volume = nominal_volume(flow=flow,
+                            v0=0.,
+                            sample_rate=sample_rate)
+                        
+    pressure = nominal_pressure(volume=volume,
+                                peep=peep,
+                                compliance_func=constant_compliance)
+        
+    time = numpy.arange(0,sim_time,1.0/sample_rate)
+    import pylab
+
+    fig=pylab.figure()
+    pylab.subplot(3,1,1)
+    pylab.plot(time,flow)
+    pylab.xlabel("Time (seconds)")
+    pylab.ylabel("Flow (L/m)")
+
+    pylab.subplot(3,1,2)
+    pylab.plot(time,volume)
+    pylab.xlabel("Time (seconds)")
+    pylab.ylabel("Volume (L)")
+
+    pylab.subplot(3,1,3)
+    pylab.plot(time,pressure)
+    pylab.xlabel("Time (seconds)")
+    pylab.ylabel("Pressure (cm H2O)")
+
+
+    pylab.show()
