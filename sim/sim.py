8befe84347ccca6fc49495f943f1a347428d7a56
Henry Schreiner <henryschreineriii@gmail.com>
2020-04-06 21:09:16 -0400

    Numpy as np

--- sim/sim.py
+++ sim/sim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import numpy
+import numpy as np
 
 
 def constant_compliance(**kwargs):
@@ -22,7 +22,7 @@ def get_breath_starts(*, max_time, breathing_rate):
         1.2 * max_time / breathing_rate
     )  # safety margin for fluctuating this later
 
-    breath_starts = numpy.arange(1, 1 + max_breaths * breathing_rate, breathing_rate)
+    breath_starts = np.arange(1, 1 + max_breaths * breathing_rate, breathing_rate)
 
     return breath_starts  # [breath_starts<max_time]
 
@@ -49,7 +49,7 @@ def nominal_flow(
     """
 
     bins = int(sim_time * sampling_rate)
-    flow = numpy.zeros(bins)
+    flow = np.zeros(bins)
 
     flow_start_bins = breath_starts * sampling_rate
     # max_flow is in L/minute
@@ -57,29 +57,29 @@ def nominal_flow(
 
     flow_start_bins = flow_start_bins.astype(int)
     flow_end_bins = flow_end_bins.astype(int)
-    breath_integrals = numpy.zeros(len(flow_start_bins))
+    breath_integrals = np.zeros(len(flow_start_bins))
 
     for i in range(len(flow_start_bins)):
         if flow_start_bins[i] < bins:
             flow[flow_start_bins[i] : min(bins, flow_end_bins[i])] = max_flow
-            breath_integrals[i] = numpy.sum(
+            breath_integrals[i] = np.sum(
                 flow[flow_start_bins[i] : min(bins, flow_end_bins[i])]
             )
 
     exp_zero_bins = flow_start_bins[1:]
     exp_min_bins = flow_end_bins[:-1]
-    times = numpy.arange(0, 1.2 * sim_time, 1.0 / sampling_rate)
+    times = np.arange(0, 1.2 * sim_time, 1.0 / sampling_rate)
 
     for i in range(len(exp_min_bins)):
         if exp_min_bins[i] < bins:
             b_min = exp_min_bins[i] + 2
             b_max = min(bins, exp_zero_bins[i]) - 1
-            flow[b_min:b_max] = numpy.log(
+            flow[b_min:b_max] = np.log(
                 (times[b_min:b_max] - times[flow_start_bins[i]])
                 / (times[b_max] - times[flow_start_bins[i]])
             )
             flow[b_min:b_max] *= (
-                -1.0 * breath_integrals[i] / numpy.sum(flow[b_min:b_max])
+                -1.0 * breath_integrals[i] / np.sum(flow[b_min:b_max])
             )
 
     return flow
@@ -92,7 +92,7 @@ def nominal_volume(*, flow, v0, sample_rate):
     v0
     sample_rate
     """
-    return numpy.cumsum(flow) * (1.0 / sample_rate) + v0
+    return np.cumsum(flow) * (1.0 / sample_rate) + v0
 
 
 def nominal_pressure(*, volume, peep, compliance_func):
@@ -105,10 +105,10 @@ def nominal_pressure(*, volume, peep, compliance_func):
 
     # We can add **kwargs and join it with locals if we need to go further up the chain
     compliance_val = compliance_func(**locals())
-    pressure = numpy.zeros(len(volume))
+    pressure = np.zeros(len(volume))
     pressure[0] = peep
     delta_p = (volume[1:] - volume[:-1]) * compliance_val
-    pressure[1:] = pressure[0] + numpy.cumsum(delta_p)
+    pressure[1:] = pressure[0] + np.cumsum(delta_p)
     return pressure
 
 
@@ -139,7 +139,7 @@ def make(
         volume=volume, peep=peep, compliance_func=constant_compliance
     )
 
-    time = numpy.arange(0, sim_time, 1.0 / sample_rate)
+    time = np.arange(0, sim_time, 1.0 / sample_rate)
 
     return time, breaths, flow, volume, pressure
 
