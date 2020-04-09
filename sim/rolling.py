152056e21cba8b2d66cf91e28216d4f88b6c6ef5
Henry Schreiner <henryschreineriii@gmail.com>
2020-04-09 08:43:20 -0400

    Update rolling buffer

--- sim/rolling.py
+++ sim/rolling.py
@@ -2,70 +2,104 @@ import numpy as np
 
 
 class Rolling:
-    def __init__(self, *, size):
-        self._values = np.empty((size * 2,), dtype=np.double)
+    def __init__(self, init=None, *, window_size):
+        self._values = np.empty((window_size * 2,), dtype=np.double)
         self._start = 0
-        self.size = size
-        self.current_size = 0
+        self._window_size = window_size
+        self._current_size = 0
+
+        if init is not None:
+            self.inject(init)
+
+    @property
+    def window_size(self):
+        return self._window_size
 
     def inject(self, values):
         """
-        Add a value or an array of values to the end of the rolling buffer.
-        It is currently invalid to input a 2D array.
+        Add a value or an array of values to the end of the rolling buffer.  It
+        is currently invalid to input a 2D array.
+
+        This could be optimized or augmented in the future for faster scalar
+        addition.
         """
 
         # Make sure input is an array, truncate if larger than rolling buffer
         values = np.asarray(values)
-        if values.size > self.size:
-            values = values[-self.size :]
+        if values.size > self._window_size:
+            values = values[-self._window_size :]
         if values.ndim > 1:
             raise RuntimeError("Only 0D and 1D supported")
 
         fill_start = (
-            self._start if self.current_size == self.size else self.current_size
+            self._start
+            if self._current_size == self._window_size
+            else self._current_size
         )
 
         # Filling the first copy is always valid
         self._values[fill_start : fill_start + values.size] = values
 
         # If we go off the end, wrap around
-        overlap = fill_start + values.size - self.size
+        overlap = fill_start + values.size - self._window_size
         if overlap > 0:
-            self._values[self.size + fill_start :] = values[: self.size - fill_start]
-            self._values[:overlap] = values[self.size - fill_start :]
+            self._values[self._window_size + fill_start :] = values[
+                : self._window_size - fill_start
+            ]
+            self._values[:overlap] = values[self._window_size - fill_start :]
         else:
             self._values[
-                fill_start + self.size : fill_start + self.size + values.size
+                fill_start
+                + self._window_size : fill_start
+                + self._window_size
+                + values.size
             ] = values
 
         # Update current size and starting position
-        self.current_size = min(self.current_size + values.size, self.size)
+        self._current_size = min(self._current_size + values.size, self._window_size)
         self._start = (
-            fill_start + values.size - (self.current_size - self.size)
-        ) % self.size
+            fill_start + values.size - (self._current_size - self._window_size)
+        ) % self._window_size
 
         return self
 
     def explain(self):
+        "Gives a nice text display of the internal structure"
         mask = np.ones_like(self._values, dtype=np.bool)
-        mask[: self.current_size] = False
-        mask[self.size : self.size + self.current_size] = False
+        mask[: self._current_size] = False
+        mask[self._window_size : self._window__size + self._current_size] = False
         data = np.ma.masked_array(data=self._values, mask=mask)
         return f"{data} -> {self}"
 
     def __repr__(self):
         r = repr(np.asarray(self))
-        if "\n" in r:
-            return "Rolling(\n" + r[6:]
-        else:
-            return "Rolling(" + r[6:]
+        start = "Rolling(" + "\n" if "\n" in r else ""
+        return start + r[6:-1] + f", window_size={self._window_size})"
 
     def __str__(self):
         return str(np.array(self))
 
     def __array__(self):
-        data = self._values[self._start : self._start + self.current_size]
+        data = self._values[self._start : self._start + self._current_size]
         data.flags.writeable = False
         return data
 
-    data = __array__
+    def __getitem__(self, arg):
+        return np.asarray(self).__getitem__(arg)
+
+    def __len__(self):
+        return self._current_size
+
+
+def new_elements(rolling, addition):
+    """
+    Given two sorted arrays, find the number of elements in the second array
+    that are past the end of the first array.
+    """
+
+    final_value = rolling[-1]
+    ind = np.searchsorted(addition, final_value)
+    if ind == len(addition):
+        return 0
+    else:
+        return len(addition) - ind - (final_value == addition[ind])
--- sim/test_rolling.py
+++ sim/test_rolling.py
@@ -1,91 +1,111 @@
-from numpy.testing import assert_all_close
+from numpy.testing import assert_allclose
+import numpy as np
+
+from rolling import Rolling, new_elements
 
 
 def test_rolling_single():
-    r = Rolling(size=3)
+    r = Rolling(window_size=3)
 
-    assert r.current_size == 0
+    assert len(r) == 0
     assert r._start == 0
-    assert_all_close(r.data(), [])
+    assert_allclose(np.asarray(r), [])
 
     r.inject(1)
-    assert r.current_size == 1
+    assert len(r) == 1
     assert r._start == 0
-    assert_all_close(r.data(), [1.0])
+    assert_allclose(np.asarray(r), [1.0])
 
     r.inject(2)
-    assert r.current_size == 2
+    assert len(r) == 2
     assert r._start == 0
-    assert_all_close(r.data(), [1.0, 2.0])
+    assert_allclose(np.asarray(r), [1.0, 2.0])
 
     r.inject(3)
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [1.0, 2.0, 3.0])
+    assert_allclose(np.asarray(r), [1.0, 2.0, 3.0])
 
     r.inject(4)
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 1
-    assert_all_close(r.data(), [2.0, 3.0, 4.0])
+    assert_allclose(np.asarray(r), [2.0, 3.0, 4.0])
 
     r.inject(5)
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 2
-    assert_all_close(r.data(), [3.0, 4.0, 5.0])
+    assert_allclose(np.asarray(r), [3.0, 4.0, 5.0])
 
     r.inject(6)
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [4.0, 5.0, 6.0])
+    assert_allclose(np.asarray(r), [4.0, 5.0, 6.0])
 
     r.inject(7)
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 1
-    assert_all_close(r.data(), [5.0, 6.0, 7.0])
+    assert_allclose(np.asarray(r), [5.0, 6.0, 7.0])
 
 
 def test_rolling_double():
-    r = Rolling(size=3)
+    r = Rolling(window_size=3)
 
     r.inject([1, 2])
-    assert r.current_size == 2
+    assert len(r) == 2
     assert r._start == 0
-    assert_all_close(r.data(), [1.0, 2.0])
+    assert_allclose(np.asarray(r), [1.0, 2.0])
 
     r.inject([3, 4])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 1
-    assert_all_close(r.data(), [2.0, 3.0, 4.0])
+    assert_allclose(np.asarray(r), [2.0, 3.0, 4.0])
 
     r.inject([5, 6])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [4.0, 5.0, 6.0])
+    assert_allclose(np.asarray(r), [4.0, 5.0, 6.0])
 
 
 def test_rolling_full():
-    r = Rolling(size=3)
+    r = Rolling(window_size=3)
 
     r.inject([1, 2, 3])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [1.0, 2.0, 3.0])
+    assert_allclose(np.asarray(r), [1.0, 2.0, 3.0])
 
     r.inject([4, 5, 6])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [4.0, 5.0, 6.0])
+    assert_allclose(np.asarray(r), [4.0, 5.0, 6.0])
 
 
 def test_rolling_overlap():
-    r = Rolling(size=3)
+    r = Rolling(window_size=3)
 
     r.inject([1, 2, 3, 4])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [2.0, 3.0, 4.0])
+    assert_allclose(np.asarray(r), [2.0, 3.0, 4.0])
 
     r.inject([5, 6, 7, 8])
-    assert r.current_size == 3
+    assert len(r) == 3
     assert r._start == 0
-    assert_all_close(r.data(), [6.0, 7.0, 8.0])
+    assert_allclose(np.asarray(r), [6.0, 7.0, 8.0])
+
+
+def test_getitem():
+    r = Rolling(window_size=3)
+    r.inject([1, 2, 3])
+    assert r[0] == 1.0
+    assert_allclose(r[1:], [2.0, 3.0])
+
+
+def test_new_elements():
+    r = Rolling([1, 2, 3, 4, 5], window_size=5)
+    assert new_elements(r, [4, 5, 6, 7]) == 2
+    assert new_elements(r, [5, 6, 7]) == 2
+    assert new_elements(r, [4, 5]) == 0
+    assert new_elements(r, [2, 3]) <= 0
+    assert new_elements(r, [8, 9]) == 2
+    assert new_elements(r, [3, 8, 9]) == 2
