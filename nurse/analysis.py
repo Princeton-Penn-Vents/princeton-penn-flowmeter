5cd02b929b04873914c2b14b7d7b6c4543a8d1ec
Jim Pivarski <jpivarski@gmail.com>
2020-04-09 18:59:55 -0500

    Function to ensure that A, B, C, D always come in that order (modulo starting letter).

--- nurse/analysis.py
+++ nurse/analysis.py
@@ -27,7 +27,7 @@ def smooth_derivative(times, values, sig=0.2):
     
     return centers, intercept, slope
 
-def find_breaths(times, values, derivative, threshold=0.02):
+def find_roots(times, values, derivative, threshold=0.02):
     values_threshold = threshold*np.max(abs(values))
     derivative_threshold = threshold*np.max(abs(derivative))
 
@@ -35,8 +35,65 @@ def find_breaths(times, values, derivative, threshold=0.02):
     B, = np.nonzero((derivative[:-1] >= 0) & (derivative[1:] < 0) & (0.5*(values[:-1] + values[1:]) >= values_threshold))
     C, = np.nonzero((values[:-1] >= 0) & (values[1:] < 0) & (0.5*(derivative[:-1] + derivative[1:]) < -derivative_threshold))
     D, = np.nonzero((derivative[:-1] < 0) & (derivative[1:] >= 0) & (0.5*(values[:-1] + values[1:]) < -values_threshold))
-
+    
     return 0.5*(times[A] + times[A + 1]), 0.5*(times[B] + times[B + 1]), 0.5*(times[C] + times[C + 1]), 0.5*(times[D] + times[D + 1])
+
+def find_breaths(A, B, C, D):
+    # ensure that each type is sorted (though it probably already is)
+    A = np.sort(A)
+    B = np.sort(B)
+    C = np.sort(C)
+    D = np.sort(D)
+
+    if len(A) == 0 or len(B) == 0 or len(C) == 0 or len(D) == 0:
+        return A, B, C, D
+
+    # where does the cycle start?
+    which = np.argmin([A[0], B[0], C[0], D[0]])
+
+    ins = [A, B, C, D]
+    outs = ([], [], [], [])
+    while len(A) > 0 or len(B) > 0 or len(C) > 0 or len(D) > 0:
+        if len(ins[which]) > 1 and all(len(ins[i]) > 0 for i in range(4)):
+            nextwhich = np.argmin([ins[i][1 if i == which else 0] for i in range(4)])
+
+            # normal case: A -> B -> C -> D -> ... cycle
+            if nextwhich == (which + 1) % 4:
+                outs[which].append(ins[which][0])
+                ins[which] = ins[which][1:]
+
+            # too many of one type: A -> B -> B -> B -> C -> D -> ...
+            elif nextwhich == which:
+                combine = [ins[which][0]]
+                ins[which] = ins[which][1:]
+                while True:
+                    if np.argmin([ins[i][0] for i in range(4)]) != which:
+                        break
+                    combine.append(ins[which][0])
+                    ins[which] = ins[which][1:]
+
+                outs[which].append(np.mean(combine))
+
+            # missing one type: A -> C -> D -> ...
+            else:
+                fill_value = 0.5*(ins[which][0] + ins[nextwhich][0])
+                ins[(which + 1) % 4] = np.concatenate((np.array([fill_value]), ins[(which + 1) % 4]))
+
+                outs[which].append(ins[which][0])
+                ins[which] = ins[which][1:]
+
+        # edge condition: less than one cycle left
+        elif len(ins[which]) > 0:
+            outs[which].append(ins[which][0])
+            ins[which] = ins[which][1:]
+
+        else:
+            break
+
+        which = (which + 1) % 4
+        A, B, C, D = ins
+
+    return outs
 
 def analyze(generator):
     global favorite
@@ -49,11 +106,21 @@ def analyze(generator):
         volume = scipy.integrate.cumtrapz(flow, time / 60.0)
         pressure = generator.pressure
 
-        print(len(time))
-        if len(time) == 1500:
-            smooth_time, smooth_flow, smooth_dflow = smooth_derivative(time, flow)
-            A, B, C, D = find_breaths(smooth_time, smooth_flow, smooth_dflow)
+        # print(len(time))
+        # if len(time) == 1500:
+        #     smooth_time, smooth_flow, smooth_dflow = smooth_derivative(time, flow)
+        #     A, B, C, D = find_breaths(*find_roots(smooth_time, smooth_flow, smooth_dflow))
 
-            # open("/tmp/flow.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(time, flow)))
-            # open("/tmp/smooth_flow.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(smooth_time, smooth_flow)))
-            # raise Exception("STOP")
+        #     open("/tmp/flow.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(time, flow)))
+        #     open("/tmp/smooth_flow.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(smooth_time, smooth_flow)))
+
+        #     smooth_time, smooth_pressure, smooth_dpressure = smooth_derivative(time, pressure)
+
+        #     open("/tmp/pressure.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(time, pressure)))
+        #     open("/tmp/smooth_pressure.dat", "w").write("\n".join("%g, %g" % (x, y) for x, y in zip(smooth_time, smooth_pressure)))
+        #     open("/tmp/A.dat", "w").write("\n".join("%g, 0" % (x,) for x in A))
+        #     open("/tmp/B.dat", "w").write("\n".join("%g, 0" % (x,) for x in B))
+        #     open("/tmp/C.dat", "w").write("\n".join("%g, 0" % (x,) for x in C))
+        #     open("/tmp/D.dat", "w").write("\n".join("%g, 0" % (x,) for x in D))
+
+        #     raise Exception("STOP")
