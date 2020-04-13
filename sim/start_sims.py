562ff174d6e513216c0f515c7cc43e788a94f1cf
Henry Schreiner <henryschreineriii@gmail.com>
2020-04-13 12:45:42 -0400

    Starting toward requests

--- sim/start_sims.py
+++ sim/start_sims.py
@@ -13,14 +13,16 @@ def start_sims(nSim, start_time, sim_time):
         params = {}
 
         simulator = VentSim(start_time, sim_time)
-        simulator.load_configs(os.path.join(os.path.dirname(os.path.realpath(__file__)),"sim_configs.yml"))
-        if i==3:
+        simulator.load_configs(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), "sim_configs.yml")
+        )
+        if i == 3:
             simulator.use_config("alarm_nonbreather")
         else:
             if random.random() < 0.5:
-                simulator.use_config("nominal_breather",params)
+                simulator.use_config("nominal_breather", params)
             else:
-                simulator.use_config("nominal_nonbreather",params)
+                simulator.use_config("nominal_nonbreather", params)
 
         simulator.initialize_sim()
 
