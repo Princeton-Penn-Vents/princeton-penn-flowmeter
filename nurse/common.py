1dd0c3fd86e49bcea0727767e5777a3bd911a8bb
Henry Schreiner <henryschreineriii@gmail.com>
2020-04-17 14:54:24 -0400

    Touch up names

--- nurse/common.py
+++ nurse/common.py
@@ -12,26 +12,26 @@ guicolors = {
 }
 
 prefill = [
-    "Room 342, Joe Black, AGE 23",
-    "Room 123, Jane Green, AGE 67",
-    "Room 324, Jerry Mouse, AGE 82",
+    "Room 342, Darth Vader, AGE 45",
+    "Room 123, Frodo Baggins, AGE 50",
     "Room 243, Tom Cat, AGE 79",
-    "Room 432, Mary Jones, AGE 18",
-    "Room 654, June Adam, AGE 56",
-    "Room 102, A. Smith, AGE 94",
+    "Room 324, Jerry Mouse, AGE 82",
+    "Room 432, Mary Poppins, AGE UNKOWN",
+    "Room 654, Willow Ufgood, AGE 56",
+    "Room 102, Hermione Granger, AGE 17",
     "Room 124, UNKNOWN, AGE 60",
-    "Room 125, Gandalf the Grey, AGE 65",
+    "Room 125, Gandalf the Grey, AGE 63",
     "Room 164, Luke Skywalker, AGE 43",
     "Room 167, Indiana Jones, AGE 82",
     "Room 169, Wonder Woman, AGE 34",
-    "Room 180, Rose Flower, AGE 39",
+    "Room 180, Willy Wonka, AGE 69",
     "Room 181, Thor, AGE 700",
     "Room 182, Beaver Cleaver, AGE 62",
     "Room 183, Ebeneezer Scrooge, AGE 99",
+    "Room 002, Inigo Montoya, AGE 45",
     "Room 184, Ru N. Ning, AGE 43",
     "Room 185, O. U. Tof, AGE 50",
     "Room 186, Good Names, AGE 77",
-    "Room 187, Good Bye, AGE 59",
 ]
 
 
