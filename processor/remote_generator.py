de82f4523357bef2fdffe9e889332072fb6c3aad
Henry Schreiner <henryschreineriii@gmail.com>
2020-04-29 20:05:11 -0400

    Inject instead of copy

--- processor/remote_generator.py
+++ processor/remote_generator.py
@@ -42,7 +42,7 @@ class RemoteThread(threading.Thread):
         socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
         while not self.parent._stop.is_set():
-            socks, *_ = zmq.select([socket], [], [], 0.1)
+            socks, *_ = zmq.select([socket], [], [], 1)
             for sock in socks:
                 self._last_update = datetime.now().timestamp()
                 root = sock.recv_json()
@@ -61,14 +61,20 @@ class RemoteThread(threading.Thread):
                             logger.info("(Re)Connecting successful")
                             self.status = Status.OK
 
+            if not socks and self.status != Status.DISCON:
+                with self._remote_lock:
+                    self.status = Status.DISCON
+
     def access_collected_data(self) -> None:
         with self.parent.lock, self._remote_lock:
             self.parent._last_update = self._last_update
             self.parent._last_get = self._last_get
 
-            self.parent._time = np.asarray(self._time).copy()
-            self.parent._flow = np.asarray(self._flow).copy()
-            self.parent._pressure = np.asarray(self._pressure).copy()
+            newel = new_elements(self.parent._time, self._time)
+
+            self.parent._time.inject(self._time[-newel:])
+            self.parent._flow.inject(self._flow[-newel:])
+            self.parent._pressure.inject(self._pressure[-newel:])
 
             if self.status == Status.DISCON:
                 self.parent.status = Status.DISCON
@@ -94,9 +100,9 @@ class RemoteGenerator(Generator):
         self.status = Status.DISCON
         self._last_ts: int = 0
 
-        self._time = np.array([], dtype=np.int64)
-        self._flow = np.array([], dtype=np.double)
-        self._pressure = np.array([], dtype=np.double)
+        self._time = Rolling(window_size=Generator.WINDOW_SIZE, dtype=np.int64)
+        self._flow = Rolling(window_size=Generator.WINDOW_SIZE)
+        self._pressure = Rolling(window_size=Generator.WINDOW_SIZE)
 
         self._remote_thread: Optional[RemoteThread] = None
 
