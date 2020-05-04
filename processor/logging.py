7bddae993ea48630cf2a50ca8d64be06a60a25dd
Henry Schreiner <henryschreineriii@gmail.com>
2020-05-04 09:47:04 -0400

    Fixes to logging

--- processor/logging.py
+++ processor/logging.py
@@ -9,17 +9,17 @@ from processor.config import config
 DIR = Path(__file__).parent.resolve()
 
 
-def open_next(mypath: Path) -> TextIO:
+def open_next(mypath: Path) -> Path:
     """
     Open the next available file
     """
     i = 0
     while True:
-        try:
-            name = "{n}{i:04}{s}".format(n=mypath.stem, i=i, s=mypath.suffix)
-            new_file_path = mypath.with_name(name)
-            return open(new_file_path, "x")
-        except FileExistsError:
+        name = "{n}{i:04}{s}".format(n=mypath.stem, i=i, s=mypath.suffix)
+        new_file_path = mypath.with_name(name)
+        if not new_file_path.exists():
+            return new_file_path
+        else:
             i += 1
 
 
@@ -29,7 +29,9 @@ def init_logger(logstr: Optional[str] = None) -> None:
     """
 
     logger = logging.getLogger("povm")
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S",
+    )
 
     if logstr is None or config["global"]["debug"].get(bool):
         ch = logging.StreamHandler()
@@ -39,15 +41,8 @@ def init_logger(logstr: Optional[str] = None) -> None:
     else:
         file_path = DIR.parent / logstr
         file_path.parent.mkdir(exist_ok=True)
-        i = 0
-        while True:
-            logfile_incr = file_path.with_name(
-                f"{file_path.stem}{i:05}{file_path.suffix}"
-            )
-            if not logfile_incr.exists():
-                break
-
-        fh = logging.FileHandler(logfile_incr.resolve())
+        logfile_incr = open_next(file_path)
+        fh = logging.FileHandler(logfile_incr)
         fh.setLevel(logging.INFO)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
@@ -55,6 +50,7 @@ def init_logger(logstr: Optional[str] = None) -> None:
 
 def make_nested_logger(nested: int) -> logging.Logger:
     logger = logging.getLogger("povm")
+    print("Making nested logger", nested)
 
     if len(logger.handlers) != 1:
         raise RuntimeError("Too many handlers")
@@ -63,23 +59,25 @@ def make_nested_logger(nested: int) -> logging.Logger:
 
     nested_logger: logging.Logger = logging.getLogger(f"povm-{nested:02}")
     formatter = logging.Formatter(
-        f"%(asctime)s - %(levelname)s - {nested:02} - %(message)s"
+        f"%(asctime)s - %(levelname)s - {nested:02} - %(message)s", "%Y-%m-%d %H:%M:%S",
     )
     nested_logger.setLevel(logger.level)
 
-    if isinstance(handler, logging.StreamHandler):
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-        nested_logger.addHandler(ch)
+    if isinstance(handler, logging.FileHandler):
+        main_path = Path(handler.baseFilename).with_suffix("")
+        main_path.mkdir(exist_ok=True)
+        new_path = main_path / f"{nested:02}"
+        new_path.mkdir(exist_ok=True)
 
-    elif isinstance(handler, logging.FileHandler):
-        main_path = Path(handler.baseFilename)
-        main_path.with_suffix("") / format(nested, "02") / f"{main_path.stem}.log"
-
-        fh = logging.FileHandler(main_path)
+        fh = logging.FileHandler(new_path / f"{main_path.stem}.log")
         fh.setLevel(logging.INFO)
         fh.setFormatter(formatter)
         nested_logger.addHandler(fh)
+
+    elif isinstance(handler, logging.StreamHandler):
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        nested_logger.addHandler(ch)
 
     else:
         raise RuntimeError(f"Logger handler {handler} is not of a known type!")
