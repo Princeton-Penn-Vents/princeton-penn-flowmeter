74e320240fce2c8e58771b77bff9888059850e7a
Henry Schreiner <henryschreineriii@gmail.com>
2020-05-08 13:47:02 -0400

    Types, Blacked

--- processor/device_names.py
+++ processor/device_names.py
@@ -1,15 +1,28 @@
+from __future__ import annotations
+
 from pathlib import Path
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from typing_extensions import Final
 
 DIR = Path(__file__).parent.resolve()
 
-vendors = ["dc:a6:32:00:00:00"]   # only append to this list
+vendors = ["dc:a6:32:00:00:00"]  # only append to this list
 
 adjectives = [x.strip() for x in open(DIR.parent / "data/adjectives.txt")]
-nouns      = [x.strip() for x in open(DIR.parent / "data/nouns.txt")]
+nouns = [x.strip() for x in open(DIR.parent / "data/nouns.txt")]
+
 assert len(adjectives) == 4096
 assert len(nouns) == 4096
 
-def lcg_generator(state, multiplier=1140671485, addend=12820163, pmod=2**24):
+
+def lcg_generator(
+    state: int,
+    multiplier: int = 1140671485,
+    addend: int = 12820163,
+    pmod: int = 2 ** 24,
+) -> int:
     """
     Performs one step in a pseudorandom sequence from state (the input argument)
     to state the return value.
@@ -18,28 +31,32 @@ def lcg_generator(state, multiplier=1140671485, addend=12820163, pmod=2**24):
 
     See https://en.wikipedia.org/wiki/Linear_congruential_generator
     """
-    state = (multiplier*state + addend) % pmod
+    state = (multiplier * state + addend) % pmod
     return state
 
-def address_to_addr(address):
+
+def address_to_addr(address: str) -> int:
     """
     Converts a MAC address string (hexidecimal with colons) into an integer.
     """
     return int(address.replace(":", ""), 16)
 
-def addr_to_address(addr):
+
+def addr_to_address(addr: int) -> str:
     """
     Converts a MAC address integer (48 bit) into a string (lowercase hexidecimal
     with colons).
     """
     out = hex(addr).replace("0x", "")
-    return ":".join(out[2*i:2*i + 2] for i in range(6))
+    return ":".join(out[2 * i : 2 * i + 2] for i in range(6))
 
-vendor_mask    = 0xffffff000000
-adjective_mask = 0x000000fff000
-noun_mask      = 0x000000000fff
 
-def address_to_name(address):
+vendor_mask: Final[int] = 0xFFFFFF000000
+adjective_mask: Final[int] = 0x000000FFF000
+noun_mask: Final[int] = 0x000000000FFF
+
+
+def address_to_name(address: str) -> str:
     """
     Converts a MAC address string (hexidecimal with colons) into a two-word name.
 
@@ -65,7 +82,7 @@ def address_to_name(address):
     out a line on a word that leads to the loop.
     """
     addr = address_to_addr(address)
-    state = (addr & ~vendor_mask)
+    state = addr & ~vendor_mask
 
     for vendor_id, vendor_address in enumerate(vendors):
         vendor_addr = address_to_addr(vendor_address)
