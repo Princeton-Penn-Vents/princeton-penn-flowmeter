9764b1974e0fb4acccf009465e5505264c5c012b
Henry Schreiner <henryschreineriii@gmail.com>
2020-05-07 21:59:18 -0400

    Try mac in address

--- processor/broadcast.py
+++ processor/broadcast.py
@@ -63,7 +63,7 @@ class Broadcast:
 
             self.info = ServiceInfo(
                 "_http._tcp.local.",
-                f"Princeton Open Vent Monitor {self.port}._http._tcp.local.",
+                f"Princeton Open Vent Monitor {get_mac_addr()} {self.port}._http._tcp.local.",
                 addresses=[ipaddress.ip_address(ip).packed for ip in addrs],
                 port=self.port,
                 properties={
