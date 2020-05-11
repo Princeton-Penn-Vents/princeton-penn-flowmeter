f6852df104a5390e9040dfab55d93b225f721033
Henry Schreiner <henryschreineriii@gmail.com>
2020-05-11 17:39:01 -0400

    Use exit stack, nicer startup display

--- patient_loop.py
+++ patient_loop.py
@@ -10,21 +10,21 @@ args = parser.parse_args()
 import signal
 import threading
 from pathlib import Path
+from contextlib import ExitStack
+
 
 from processor.settings import get_live_settings
 from patient.rotary_lcd import RotaryLCD
 from processor.collector import Collector
 from processor.broadcast import Broadcast
+from patient.mac_address import get_box_name
 
 DIR = Path(__file__).parent.resolve()
 
 
 # Initialize LCD
-with RotaryLCD(get_live_settings()) as rotary, Collector(
-    rotary=rotary, port=args.port
-) as collector, Broadcast("patient_loop", port=args.port, live=5):
-    rotary.live_load(DIR / "povm-live.yml")
-    rotary.live_save(DIR / "povm-live.yml", every=10)
+with ExitStack() as stack:
+    rotary = stack.enter_context(RotaryLCD(get_live_settings()))
 
     forever = threading.Event()
 
@@ -33,6 +33,20 @@ with RotaryLCD(get_live_settings()) as rotary, Collector(
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         forever.set()
+
+    rotary.lcd.upper("POVM Box name:")
+    rotary.lcd.lower("Getting name...")
+    rotary.lcd.lower(f"{get_box_name():<20}")
+    forever.wait(3)
+    rotary.lcd.upper("Turn to select alarm ")
+    rotary.lcd.lower("Push and turn to set ")
+    forever.wait(2)
+
+    collector = stack.enter_context(Collector(rotary=rotary, port=args.port))
+    stack.enter_context(Broadcast("patient_loop", port=args.port, live=5))
+
+    rotary.live_load(DIR / "povm-live.yml")
+    rotary.live_save(DIR / "povm-live.yml", every=10)
 
     signal.signal(signal.SIGINT, close)
     signal.signal(signal.SIGTERM, close)
