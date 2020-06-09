eec07f3e9b66802ed1806a0ff8fe5f3567ce38d0
David Lange <david.lange@cern.ch>
2020-06-09 16:01:38 +0200

    add extrapolation

--- processor/flow_calibrator.py
+++ processor/flow_calibrator.py
@@ -24,16 +24,31 @@ class flow_calibrator:
             self.func = self.simple
         if block.endswith("yaml") or block.endswith("yml"):
             qs, deltaPs = get_yaml(block)
-            self.func = scipy.interpolate.interp1d(
+            self.interp = scipy.interpolate.interp1d(
                 deltaPs, qs, kind="cubic", fill_value="extrapolate"
             )
-
+            self.func = self.extrap1d
+            
         assert self.func is not None
 
     def simple(self, deltaP):
         #        return np.copysign(np.abs(deltaP) ** (4 / 7),deltaP)*0.7198/0.09636372314370535
         return np.copysign(np.abs(deltaP / 0.02962975) ** (4 / 7), deltaP)
 
+    def extrap1d(self, deltaP):
+        retval = self.interp(deltaP)
+        xs = self.interp.x
+        print(xs[-1])
+        print(self.interp(xs[-1]))
+        if isinstance(retval, np.ndarray):
+            beyond = deltaP > xs[-1]
+            retval[beyond] = self.interp(xs[-1])*np.power(deltaP[beyond] /xs[-1], 0.5 ) # 4 / 7 is an alternative
+        else:
+            if deltaP > xs[-1]:
+                retval = self.interp(xs[-1])*np.power(deltaP / xs[-1], 0.5 ) # 4 / 7 is an alternative
+                
+        return retval
+    
     def Q(self, f) -> np.ndarray or float:
         return self.func(f / 60.0)  # put f into Pa to get deltaP
 
@@ -53,7 +68,7 @@ if __name__ == "__main__":
     caliber2 = flow_calibrator(
        os.path.join(os.path.dirname(__file__), "flowcalib_ave.yaml")
     )
-    fs = np.arange(0.0, 25000.0, 10.0)
+    fs = np.arange(0.0, 33000.0, 10.0) #above the max of the ADC
     res2 = caliber2.Q(fs)
     
     print("Checking the detailed input file")
@@ -64,8 +79,8 @@ if __name__ == "__main__":
 
     import pylab
 
-    pylab.plot(fs,res2,label="Ave")
-    pylab.plot(fs,res3,label="All")
+    pylab.plot(fs,res2,label="Spline + power of 2 above 100 L/min")
+    pylab.plot(fs,res3,label="Spline from Philippe")
     pylab.xlabel('measured f')
     pylab.ylabel('Q (L/min)')
     pylab.legend(loc='best')
