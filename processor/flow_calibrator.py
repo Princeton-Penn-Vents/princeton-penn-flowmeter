8a1359ee856faebbfbe7f895a6e3834a69d7da8c
David Lange <david.lange@cern.ch>
2020-06-09 09:44:10 +0200

    first lut version

--- processor/flow_calibrator.py
+++ processor/flow_calibrator.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import scipy.interpolate
+import yaml
 
 
-def simple(f):
-    return np.abs(f) ** (4 / 7)
+def get_yaml(yml_file):
+    stream = open(yml_file, "r")
+    params = yaml.safe_load(stream)
+    qs = np.zeros(len(params))
+    deltaPs = np.zeros(len(params))
+    for i, d in enumerate(params):
+        qs[i] = d["Q"]
+        deltaPs[i] = d["deltaP"]
+
+    return qs, deltaPs
 
 
 class flow_calibrator:
     def __init__(self, block="simple") -> None:
         self.func = None
         if block == "simple":
-            self.func = simple
+            self.func = self.simple
+        if block.endswith("yaml") or block.endswith("yml"):
+            qs, deltaPs = get_yaml(block)
+            self.func = scipy.interpolate.interp1d(
+                deltaPs, qs, kind="cubic", fill_value="extrapolate"
+            )
 
         assert self.func is not None
 
-    def deltaP(self, f) -> np.ndarray or float:
-        return self.func(f)
+    def simple(self, deltaP):
+        #        return np.copysign(np.abs(deltaP) ** (4 / 7),deltaP)*0.7198/0.09636372314370535
+        return np.copysign(np.abs(deltaP / 0.02962975) ** (4 / 7), deltaP)
+
+    def Q(self, f) -> np.ndarray or float:
+        return self.func(f / 60.0)  # put f into Pa to get deltaP
 
 
 if __name__ == "__main__":
@@ -24,8 +43,15 @@ if __name__ == "__main__":
     caliber = flow_calibrator()
     fs = np.arange(0.0, 110.0, 10.0)
     for f in fs:
-        print(f, caliber.deltaP(f))
+        print(f, caliber.Q(f))
 
     print("Checking array syntax too")
     print(fs)
-    print(caliber.deltaP(fs))
+    print(caliber.Q(fs))
+
+    print("Checking the average input file")
+    caliber2 = flow_calibrator(
+        "/Users/lange/covid/gui/princeton-penn-flowmeter/processor/flowcalib_ave.yaml"
+    )
+    print(fs)
+    print(caliber2.Q(fs))
