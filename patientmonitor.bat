39b0b1e659e6980c2d43b64146b1562625507664
Christopher Tully <Chris.Tully@cern.ch>
2020-10-24 13:13:45 -0400

    switch from debug to fresh in Windows batch file

--- patientmonitor.bat
+++ patientmonitor.bat
@@ -1,3 +1,3 @@
 call %USERPROFILE%\Miniconda3\condabin\activate.bat flowmeter
-python %USERPROFILE%\princeton-penn-flowmeter\nursegui.py --debug
+python %USERPROFILE%\princeton-penn-flowmeter\nursegui.py --fresh
 call conda deactivate
